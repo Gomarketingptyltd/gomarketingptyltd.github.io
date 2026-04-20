9b9f420c9920cecc115a44617e0df6d6f95a2424
Rosy Yu <rosyyu@iMac.mymodem>
2026-04-20 14:12:39 +1000

    Clean up service pages and SEO basics

--- js/drop-down-up.js
+++ js/drop-down-up.js
@@ -1,13 +1,15 @@
 const down = document.getElementById("drop-down");
 const up = document.getElementById("drop-up");
 const services = document.getElementById("menu-services");
-down.addEventListener("click", () => {
-    down.style.display = "none";
-    up.style.display = "block";
-    services.style.display = "flex";
-})
-up.addEventListener("click", () => {
-    down.style.display = "block";
-    up.style.display = "none";
-    services.style.display = "none";
-})
+if (down && up && services) {
+    down.addEventListener("click", () => {
+        down.style.display = "none";
+        up.style.display = "block";
+        services.style.display = "flex";
+    })
+    up.addEventListener("click", () => {
+        down.style.display = "block";
+        up.style.display = "none";
+        services.style.display = "none";
+    })
+}
